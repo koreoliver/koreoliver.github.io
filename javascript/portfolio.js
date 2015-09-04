db3eb395365dcd9c4f10c3b61f670809d38fa723
koreoliver <kore.oliver@gmail.com>
2015-09-04 11:57:45 -0700

    update to javascript

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,5 +1,5 @@
 
-document.document.body.form('button').onclick = function() {
+/*document.document.body.form('button').onclick = function() {
     document.document.body.form('login').style.visibility = 'visible';
 }
 document.document.body.form('closebutton').onclick = function() {
@@ -7,8 +7,8 @@ document.document.body.form('closebutton').onclick = function() {
 }
 
 if (document.body.form['form']['name'] == null){ alert("Please enter your name")} 
-else(document.body.form['form']['email'] == null){ alert("Please enter your email")}
-else(document.body.form['form']['message'] == null){ alert("Please enter a message")}  
-else if {print("h5")};
+else if(document.body.form['form']['email'] == null){ alert("Please enter your email")}
+else if(document.body.form['form']['message'] == null){ alert("Please enter a message")}  
+else {print("h5")};
 
 

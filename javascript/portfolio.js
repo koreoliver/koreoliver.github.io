bb3b83bbfc306e3dc396593d75ceb313903c3ecd
koreoliver <kore.oliver@gmail.com>
2015-09-06 14:21:08 -0700

    form validation

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,14 +1,52 @@
+/*----------------------------------------------------------------------------- 
+01=CONTACT FORM SUMIT
+-----------------------------------------------------------------------------*/
+var hiddenSubmit = document.getElementsbyClassName(hidden);
 
-/*document.document.body.form('button').onclick = function() {
-    document.document.body.form('login').style.visibility = 'visible';
+document.getElementByClassName("submit").addEventListener("click",hiddenSubmit);
+
+function () 
+button.addEventListener("onsend").style.visibility = 'visible';
+button.addEventListener("window.close();").style.visibility = 'hidden';
+
+
+/*function myFunction() {
+	
+if (document.getElementByTagName(name) === null) 
+	alert("Please enter your name");	
+else if (document.body.form.email === null) 
+	alert("Please enter your email");	
+else if (document.getElementByTagName('message') === null) 
+	alert("Please enter a message");
+else (document.getElementsbyClassName('hidden').style.visibility = 'visible')
+	    
 }
-document.document.body.form('closebutton').onclick = function() {
-    document.document.body.form('login').style.visibility = 'hidden';
-}
 
-if (document.body.form['form']['name'] == null){ alert("Please enter your name")} 
-else if(document.body.form['form']['email'] == null){ alert("Please enter your email")}
-else if(document.body.form['form']['message'] == null){ alert("Please enter a message")}  
-else {print("h5")};
 
+(function() {
+    // FORM / CONTACT
+    var $contactForm = $('#contactForm');
+    // VALIDATE FORM
+    $contactForm.validate();
+    $contactForm.submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://formspree.io/kore.oliver@gmail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                $contactForm.append('<div class="message message--loading">Sending message…</div>');
+            },
+            success: function(data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--success">Message sent!</div>');
+            },
+            error: function(err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="message message--error">Ops, there was an error.</div>');
+            }
+        });
+    });
+})();
 

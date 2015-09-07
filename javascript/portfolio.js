1fced10e71e8a547cf7bf88479ad57b061f36250
koreoliver <kore.oliver@gmail.com>
2015-09-07 15:00:07 -0700

    update contact page

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,10 +1,3 @@
 /*----------------------------------------------------------------------------- 
 01=CONTACT FORM SUMIT
 -----------------------------------------------------------------------------*/
-$(function(){
-    $("#big-image img:eq(0)").nextAll().hide();
-    $(".small-images img").click(function(e){
-        var index = $(this).index();
-        $("#big-image img").eq(index).show().siblings().hide();
-    });
-});

d92d06eafcd25f3ef98e6d0436b5acf091b851eb
koreoliver <kore.oliver@gmail.com>
2015-09-07 17:09:17 -0700

    make message area required

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,6 +1,14 @@
 /*----------------------------------------------------------------------------- 
 01=CONTACT FORM SUMIT
 -----------------------------------------------------------------------------*/
+/*function showDiv() {
+   document.getElementByClassName('hidden').style.display = "block";
+}
+
+function hideDiv() {
+   document.getElementByClassName('hidden').style.display = "block";
+}
+
 document.getElementByClassName("hidden").addEventListener("submit"),
             function(event) {
                 event.preventDefault();

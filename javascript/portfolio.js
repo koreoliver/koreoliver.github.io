29ba4496e339e803190fbbb46ce8188938b914ac
koreoliver <kore.oliver@gmail.com>
2015-09-23 11:28:39 -0700

    add jquery

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,42 +1,9 @@
 /*----------------------------------------------------------------------------- 
-01=CONTACT FORM SUBMIT
+01=NAV BAR
 -----------------------------------------------------------------------------*/
-/*$('.nav-container li').click(function() {
+/*jQuery(function(){
+  $('.nav-container li').click(function() {
     $(this).addClass('highlight').siblings().removeClass('highlight');
 });
-
-
-/*$('#projectimagestest').on('click', function(e) {
-    $(this).toggleClass('fullSize');
 });
-
-
-
-/*$(".projectimagestest").toggle(function()
-    {$(this).animate({width: "1000px"}, 'slow');},
-    function()
-    {$(this).animate({width: "120px"}, 'slow');
-});
-
-
-$(document).ready(function(){
-TriggerClick = 0;
-
-$(".projectimages").click(function(){
- if(TriggerClick==0){
-     TriggerClick=1;
-     $("div#test").animate({width:'1000px'}, 500);
- }else{
-     TriggerClick=0;
-     $("div#test").animate({width:'300px'}, 500);
- };
- });
-});
-
-
-
-
-
-
-
 
